315a419fa89172972dd289acaccae03db279e920
tripathipriyanshu20 <135949955+tripathipriyanshu20@users.noreply.github.com>
2023-09-29 18:40:13 +0530

    Add files via upload

---
+++ movie-catalog-service/src/main/java/io/project/moviecatalogservice/models/CatalogItem.java
@@ -0,0 +1,36 @@
+package io.project.moviecatalogservice.models;
+
+public class CatalogItem {
+
+    private String name;
+    private String desc;
+    private String rating;
+    public CatalogItem(String name, String desc, String rating){
+        this.name= name;
+        this.desc= desc;
+        this.rating= rating;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
---
+++ movie-catalog-service/src/main/java/io/project/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package io.project.moviecatalogservice.resources;
+
+import io.project.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+
+
+        return Collections.singletonList(
+                new CatalogItem("Transformers", "Test", "4")
+        );
+    }
+}
